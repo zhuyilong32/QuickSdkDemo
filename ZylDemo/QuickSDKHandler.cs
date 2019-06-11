a90186121dfd08d906d09755459f071941a49a8c
zhuyilong32 <46468496+zhuyilong32@users.noreply.github.com>
2019-06-11 10:22:18 +0800

    添加Asset

---
+++ ZylDemo/QuickSDKHandler.cs
@@ -0,0 +1,139 @@
+﻿using UnityEngine;
+using System.Collections;
+using quicksdk;
+using UnityEngine.UI;
+public class QuickSDKHandler : QuickSDKListener
+{
+    Button m_Login;
+    Button m_Pay;
+    Button m_Enter;
+    void Awake()
+    {
+        QuickSDK.getInstance().setListener(this);
+        m_Login = GameObject.Find("Canvas/Login").GetComponent<Button>();
+        m_Login.onClick.AddListener(OnLoginBtnDown);
+        m_Pay= GameObject.Find("Canvas/Pay").GetComponent<Button>();
+        m_Pay.onClick.AddListener(OnPayDown);
+        m_Enter = GameObject.Find("Canvas/Enter").GetComponent<Button>();
+        m_Enter.onClick.AddListener(OnEnterDown);
+    }
+
+    void showLog(string title, string message)
+    {
+        Debug.Log("title: " + title + ", message: " + message);
+    }
+
+    //callback
+    public override void onInitSuccess()
+    {
+        showLog("onInitSuccess", "");
+        //QuickSDK.getInstance ().login (); //如果游戏需要启动时登录，需要在初始化成功之后调用
+        QuickSDKHelper.SetOrderInfo();
+        QuickSDKHelper.SetRoleInfo();
+    }
+
+    public override void onInitFailed(ErrorMsg errMsg)
+    {
+        showLog("onInitFailed", "msg: " + errMsg.errMsg);
+    }
+
+    public override void onLoginSuccess(UserInfo userInfo)
+    {
+        showLog("onLoginSuccess", "uid: " + userInfo.uid + " ,username: " + userInfo.userName + " ,userToken: " + userInfo.token + ", msg: " + userInfo.errMsg);
+    }
+
+    public override void onSwitchAccountSuccess(UserInfo userInfo)
+    {
+        //切换账号成功，清除原来的角色信息，使用获取到新的用户信息，回到进入游戏的界面，不需要再次调登录
+        showLog("onLoginSuccess", "uid: " + userInfo.uid + " ,username: " + userInfo.userName + " ,userToken: " + userInfo.token + ", msg: " + userInfo.errMsg);
+    }
+
+    public override void onLoginFailed(ErrorMsg errMsg)
+    {
+        showLog("onLoginFailed", "msg: " + errMsg.errMsg);
+    }
+
+    public override void onLogoutSuccess()
+    {
+        showLog("onLogoutSuccess", "");
+        //注销成功后回到登陆界面0
+        //Application.LoadLevel("scene1");
+    }
+
+
+
+    public override void onPaySuccess(PayResult payResult)
+    {
+        showLog("onPaySuccess", "orderId: " + payResult.orderId + ", cpOrderId: " + payResult.cpOrderId + " ,extraParam" + payResult.extraParam);
+    }
+
+    public override void onPayCancel(PayResult payResult)
+    {
+        showLog("onPayCancel", "orderId: " + payResult.orderId + ", cpOrderId: " + payResult.cpOrderId + " ,extraParam" + payResult.extraParam);
+    }
+
+    public override void onPayFailed(PayResult payResult)
+    {
+        showLog("onPayFailed", "orderId: " + payResult.orderId + ", cpOrderId: " + payResult.cpOrderId + " ,extraParam" + payResult.extraParam);
+    }
+
+    public override void onExitSuccess()
+    {
+        showLog("onExitSuccess", "");
+        //退出成功的回调里面调用  QuickSDK.getInstance ().exitGame ();  即可实现退出游戏，杀进程。为避免与渠道发生冲突，请不要使用  Application.Quit ();
+        QuickSDK.getInstance().exitGame();
+    }
+
+    /// <summary>
+    /// 登陆时候调用
+    /// </summary>
+    void Login()
+    {
+        QuickSDK.getInstance().login();
+    }
+
+    public void OnLoginBtnDown()
+    {
+        Login();
+        Debug.Log("LoginBtnDown");
+    }
+
+    public void OnPayDown()
+    {
+        Pay(QuickSDKHelper.GetOrder,QuickSDKHelper.GetRole);
+        Debug.Log("PayBtnDown");
+    }
+
+    public void OnEnterDown()
+    {
+        QuickSDK.getInstance().enterGame(QuickSDKHelper.GetRole);
+        Debug.Log("EnterBtnDown");
+    }
+
+    void CreateRole()
+    {
+        QuickSDK.getInstance().createRole(QuickSDKHelper.GetRole);//创建角色
+    }
+    //进入游戏上传
+    void EnterGame(GameRoleInfo gameRoleInfo)
+    {
+        QuickSDK.getInstance().enterGame(gameRoleInfo);
+    }
+
+    void updateRole(GameRoleInfo gameRoleInfo)
+    {
+        QuickSDK.getInstance().updateRole(gameRoleInfo);
+    }
+
+    void Pay(OrderInfo orderInfo, GameRoleInfo gameRoleInfo)
+    {
+        QuickSDK.getInstance().pay(orderInfo, gameRoleInfo);
+    }
+
+    void logOut()
+    {
+        QuickSDK.getInstance().logout();
+    }
+
+
+}
---
+++ ZylDemo/QuickSDKHelper.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using quicksdk;
+public static class QuickSDKHelper {
+    private static GameRoleInfo m_Role=new GameRoleInfo();
+    private static OrderInfo m_Order=new OrderInfo();
+    public static GameRoleInfo GetRole
+    {
+        get {
+            return m_Role;
+        }
+    }
+
+    public static OrderInfo GetOrder
+    {
+        get
+        {
+            return m_Order;
+        }
+    }
+
+    public static void SetRoleInfo()
+    {
+        m_Role.gameRoleBalance = "0";
+        m_Role.gameRoleID = "000001";
+        m_Role.gameRoleLevel = "1";
+        m_Role.gameRoleName = "钱多多";
+        m_Role.partyName = "同济会";
+        m_Role.serverID = "1";
+        m_Role.serverName = "火星服务器";
+        m_Role.vipLevel = "1";
+        m_Role.roleCreateTime = "roleCreateTime";//UC，当乐与1881渠道必传，值为10位数时间戳
+        m_Role.gameRoleGender = "男";//360渠道参数
+        m_Role.gameRolePower = "38";//360渠道参数，设置角色战力，必须为整型字符串
+        m_Role.partyId = "1100";//360渠道参数，设置帮派id，必须为整型字符串
+        m_Role.professionId = "11";//360渠道参数，设置角色职业id，必须为整型字符串
+        m_Role.profession = "法师";//360渠道参数，设置角色职业名称
+        m_Role.partyRoleId = "1";//360渠道参数，设置角色在帮派中的id
+        m_Role.partyRoleName = "帮主"; //360渠道参数，设置角色在帮派中的名称
+        m_Role.friendlist = "无";//360渠道参数，设置好友关系列表，格式请参考：http://open.quicksdk.net/help/detail/aid/190
+    }
+
+    public static void SetOrderInfo()
+    {
+        m_Order.goodsID = "1";
+        m_Order.goodsName = "勾玉";
+        m_Order.amount = 1;
+        m_Order.count = 10;
+        m_Order.cpOrderID = "cporderidzzw";
+        m_Order.extrasParams = "extparma";
+        m_Order.price = 0.1f;  //停用的，不用给值
+        m_Order.quantifier = "个";  //停用的，不用给值
+        m_Order.goodsDesc = "10个勾玉";  //停用的，不用给值
+    }
+}
